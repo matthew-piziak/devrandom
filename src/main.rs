89ff88a4b362364863ff97b90fcc9986624b8672
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-15 10:55:05 -0500

    extend module docstring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-//! A clone of `$ cat /dev/random`
+//! A clone of `$ cat /dev/random`, i.e. a blocking pseudorandom number
+//! generator which interfaces with environmental noise to gather entropy.
 
 fn main() {
     println!("Hello, world!");
