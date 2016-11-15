ca3b8ea3effccccbe212beb012a4e534c2af38c3
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-15 11:46:36 -0500

    add mock blocking generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 //! A clone of `$ cat /dev/random`, i.e. a blocking pseudorandom number
-//! generator which interfaces with environmental noise to gather entropy.
+//! generator which gathers randomness from environmental noise.
+
+extern crate rand;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    let mut entropy_count = 0;
+    loop {
+        let mut rng = rand::thread_rng();
+        let random_number: u8 = rng.gen();
+        if rng.gen() {
+            entropy_count += 1;
+        }
+        if entropy_count > 0 {
+            entropy_count -= 1;
+            println!("{}", random_number);
+        } else {
+            println!("Blocking");
+        }
+    }
 }
