5343c0d190b3a8ecadc43207f3d0c90f36554e80
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-16 15:39:01 -0500

    experiment with streams

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,42 @@
 //! - Randomness source
 //! - Debiaser (von Neumann whitening?)
 //! - Cryptographically secure pseudorandom number generator (CSPRG)
-//!
-//! TODO: How to represent a bitstream in Rust?
 
-extern crate rand;
-use rand::Rng;
+extern crate futures;
+extern crate tokio_core;
+
+use futures::stream;
+use tokio_core::reactor;
 
 fn main() {
-    let mut entropy_count = 0;
-    loop {
-        let mut rng = rand::thread_rng();
-        let random_number: u8 = rng.gen();
-        if rng.gen() {
-            entropy_count += 1;
-        }
-        if entropy_count > 0 {
-            entropy_count -= 1;
-            println!("{}", random_number);
-        } else {
-            println!("Blocking");
-        }
+    let randomness_source: RandomnessStream = MockRandomnessSource::new(vec![true, false, true])
+        .stream();
+
+    randomness_source.for_each(|r| println!("{}", r))
+}
+
+type RandomnessStream = stream::BoxStream<bool, RandomnessSourceError>;
+
+enum RandomnessSourceError {}
+
+trait RandomnessSource {
+    fn stream(&self) -> RandomnessStream;
+}
+
+struct MockRandomnessSource {
+    random_data: Vec<bool>,
+}
+
+impl MockRandomnessSource {
+    fn new(random_data: Vec<bool>) -> Self {
+        MockRandomnessSource { random_data: random_data }
+    }
+}
+
+impl RandomnessSource for MockRandomnessSource {
+    fn stream(&self) -> RandomnessStream {
+        let results: Vec<Result<bool, RandomnessSourceError>> =
+            self.random_data.clone().into_iter().map(Ok).collect();
+        Box::new(stream::iter(results))
     }
 }
