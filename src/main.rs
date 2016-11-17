5b8e3423c4ba82e19da43a0b846a91ad275e7f96
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-16 21:55:43 -0500

    experiment with streams

--- src/main.rs
+++ src/main.rs
@@ -8,55 +8,51 @@
 //! - Cryptographically secure pseudorandom number generator (CSPRG)
 //! - Entropy Counter
 
+extern crate futures;
 extern crate rand;
 
-use std::sync::mpsc;
 use std::thread;
 
+use futures::stream::Stream;
+use futures::stream;
+use futures::task;
 use rand::Rng;
 
 fn main() {
-    let (randomness_transmitter, randomness_receiver) = mpsc::channel();
-
-    let randomness_transmitter = randomness_transmitter.clone();
+    let (randomness_transmitter, randomness_receiver) = stream::channel<bool, bool>();
 
     thread::spawn(move || {
         loop {
             let mut rng = rand::thread_rng();
             let bit: bool = rng.gen() && rng.gen(); // biased boolean generator
-            match randomness_transmitter.send(bit) {
-                Ok(_) => {}
-                Err(_) => {
-                    break;
-                }
-            }
+            randomness_transmitter.send(Ok(bit));
         }
     });
 
-    let (unbiased_transmitter, unbiased_receiver) = mpsc::channel();
+    // let (unbiased_transmitter, unbiased_receiver) = stream::channel();
 
-    thread::spawn(move || loop {
-        let i = randomness_receiver.recv().unwrap();
-        let j = randomness_receiver.recv().unwrap();
-        if i == j {
-            continue;
-        } else {
-            match unbiased_transmitter.send(i) {
-                Ok(_) => {}
-                Err(_) => {
-                    break;
-                }
-            }
-        }
-    });
+    // thread::spawn(move || loop {
+    //     let i = randomness_receiver.recv().unwrap();
+    //     let j = randomness_receiver.recv().unwrap();
+    //     if i == j {
+    //         continue;
+    //     } else {
+    //         match unbiased_transmitter.send(i) {
+    //             Ok(_) => {}
+    //             Err(_) => {
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // });
 
     loop {
         let mut byte: u8 = 0;
         for _ in 0..8 {
             byte <<= 1;
-            if unbiased_receiver.recv().unwrap() {
+            if let Ok(r) = randomness_receiver.poll() {
                 byte += 1;
-            };
+            }
         }
         use std::io::{self, Write};
         io::stdout().write(&[byte]);
