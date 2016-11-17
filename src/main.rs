6dfafbc03398d4be0db2b8d3d18ee4ab8869268d
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-16 23:42:46 -0500

    experiment with folding bool streams

--- src/main.rs
+++ src/main.rs
@@ -13,49 +13,14 @@ extern crate rand;
 
 use std::thread;
 
-use futures::stream::Stream;
-use futures::stream;
-use futures::task;
+use futures::stream::{self, Stream};
+use futures::future::{finished, Future};
 use rand::Rng;
 
 fn main() {
-    let (randomness_transmitter, randomness_receiver) = stream::channel<bool, bool>();
-
-    thread::spawn(move || {
-        loop {
-            let mut rng = rand::thread_rng();
-            let bit: bool = rng.gen() && rng.gen(); // biased boolean generator
-            randomness_transmitter.send(Ok(bit));
-        }
-    });
-
-    // let (unbiased_transmitter, unbiased_receiver) = stream::channel();
-
-    // thread::spawn(move || loop {
-    //     let i = randomness_receiver.recv().unwrap();
-    //     let j = randomness_receiver.recv().unwrap();
-    //     if i == j {
-    //         continue;
-    //     } else {
-    //         match unbiased_transmitter.send(i) {
-    //             Ok(_) => {}
-    //             Err(_) => {
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // });
-
-    loop {
-        let mut byte: u8 = 0;
-        for _ in 0..8 {
-            byte <<= 1;
-            if let Ok(r) = randomness_receiver.poll() {
-                byte += 1;
-            }
-        }
-        use std::io::{self, Write};
-        io::stdout().write(&[byte]);
-        io::stdout().flush();
-    }
+    let mut rng = rand::thread_rng();
+    let random_source = rng.gen_iter().take(100).map(Ok);
+    let number_stream = stream::iter::<_, bool, ()>(random_source);
+    let sum = number_stream.fold(true, |a, b| finished(a && b));
+    println!("sum: {:?}", sum.wait());
 }
