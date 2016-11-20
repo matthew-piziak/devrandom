5343f912b22227468fc2fc116c86c132e3586742
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-20 18:58:11 -0500

    Revert "broken mouse randomness"
    This reverts commit 7a86710c6f90d9ada07c16ebf72c7f7438235a5b.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! A clone of `$ cat /dev/random`, i.e. a blocking pseudorandom number
 //! generator which gathers randomness from environmental noise.
-
+//!
 //! Architectural components:
 //! - Randomness sources
 //! - Mixer (omit for now)
@@ -20,24 +20,16 @@
 //   disclaimer only pertains to the documentation, where I believe the
 //   tradeoffs are worth it.)
 
-#![feature(conservative_impl_trait)]
-
 extern crate futures;
 extern crate rand;
 extern crate tiny_keccak;
 extern crate tokio_core;
-extern crate glfw;
 
 use futures::stream::{self, Stream, BoxStream};
 use futures::{Async, Future, Poll};
-use glfw::{Action, Context, CursorMode, Key, Window, WindowEvent, Glfw};
 use rand::Rng;
 use tiny_keccak::Keccak;
 use tokio_core::reactor::Core;
-
-use std::collections::VecDeque;
-use std::sync::mpsc::Receiver;
-use std::thread;
 
 type BitStream = BoxStream<bool, ()>;
 type ByteStream = BoxStream<u8, ()>;
@@ -45,22 +37,16 @@ type HashedStream = BoxStream<[u8; 32], ()>;
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let randomness_stream = mouse_randomness_source();
-    // core.run(randomness_stream.for_each(emit_bool)).unwrap();
-    // core.run(randomness_stream.chunks(2)
-    //                           .map(vec_to_pair)
-    //                           .filter_map(von_neumann_debias)
-    //                           .chunks(8)
-    //                           .filter_map(octet_to_byte)
-    //                           .chunks(32)
-    //                           .filter_map(sha3)
-    //                           .for_each(emit_item))
-    //     .unwrap();
-}
-
-fn emit_bool(b: bool) -> Result<(), ()> {
-    println!("bool: {}", b);
-    Ok(())
+    let randomness_stream: BitStream = mock_randomness_source();
+    core.run(randomness_stream.chunks(2)
+                              .map(vec_to_pair)
+                              .filter_map(von_neumann_debias)
+                              .chunks(8)
+                              .filter_map(octet_to_byte)
+                              .chunks(32)
+                              .filter_map(sha3)
+                              .for_each(emit_item))
+        .unwrap();
 }
 
 fn emit_item(item: [u8; 32]) -> Result<(), ()> {
@@ -70,12 +56,12 @@ fn emit_item(item: [u8; 32]) -> Result<(), ()> {
     Ok(())
 }
 
-fn mock_randomness_source() -> impl Stream<Error=(), Item=bool> {
+fn mock_randomness_source() -> BitStream {
     Box::new(RandStream { rng: rand::OsRng::new().unwrap() })
 }
 
 fn sawtooth() -> BitStream {
-    let sawtooth_stream = SawtoothStream { switch: false };
+    let sawtooth_stream = SawtoothStream {switch: false};
     println!("sawtooth");
     Box::new(sawtooth_stream)
 }
@@ -91,83 +77,6 @@ impl Stream for SawtoothStream {
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         self.switch = !self.switch;
         Ok(Async::Ready(Some(self.switch)))
-    }
-}
-
-fn mouse_randomness_source() -> impl Stream<Item=bool, Error=()> {
-    let mut mouse_randomness_source = MouseRandomnessSource::new();
-    for _ in 0..1000 {
-        mouse_randomness_source.poll();
-    }
-    mouse_randomness_source
-}
-
-struct MouseRandomnessSource {
-    glfw: Glfw,
-    window: Window,
-    events: Receiver<(f64, WindowEvent)>,
-    randomness_buffer: VecDeque<bool>,
-}
-
-impl MouseRandomnessSource {
-    fn new() -> Self {
-        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-        let (mut window, events) = glfw.create_window(800,
-                           600,
-                           "Hello, I am a window.",
-                           glfw::WindowMode::Windowed)
-            .expect("Failed to create GLFW window.");
-        window.set_cursor_mode(CursorMode::Disabled);
-        window.make_current();
-        window.set_cursor_pos_polling(true);
-        window.set_key_polling(true);
-        MouseRandomnessSource {
-            glfw: glfw,
-            window: window,
-            events: events,
-            randomness_buffer: VecDeque::new(),
-        }
-    }
-
-    fn handle_window_events(&mut self) {
-        self.glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&self.events) {
-            println!("Handling event.");
-            match event {
-                glfw::WindowEvent::CursorPos(xpos, ypos) => {
-                    let b = ((xpos + ypos) as u64 % 2) == 0;
-                    self.randomness_buffer.push_back(b);
-                    },
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => self.window.set_should_close(true),
-                glfw::WindowEvent::Key(Key::Space, _, Action::Press, _) => {
-                    match self.window.get_cursor_mode() {
-                        CursorMode::Disabled => self.window.set_cursor_mode(CursorMode::Normal),
-                        CursorMode::Normal   => self.window.set_cursor_mode(CursorMode::Disabled),
-                        _ => {}
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-}
-
-impl Stream for MouseRandomnessSource {
-    type Item = bool;
-    type Error = ();
-
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        for _ in 0..10000 {
-            if !self.window.should_close() {
-                self.handle_window_events();
-            } else {
-                break;
-            }
-        }
-        match self.randomness_buffer.pop_front() {
-            Some(event) => Ok(Async::Ready(Some(event))),
-            None => Ok(Async::NotReady),
-        }
     }
 }
 
